e85b8fd83994aaebfcf7cd976082faf2983365dc
abdil065 <abdil065@umn.com>
2024-04-23 19:03:42 +0000

    my commit on lab 9

---
+++ lamp_stack_server.pp
@@ -0,0 +1,30 @@
+package { 'apache2':
+  ensure => installed,
+}
+package { 'php':
+  ensure => installed,
+  notify  => Service['apache2'],
+  require => [Package['apache2']],
+}
+service { 'apache2':
+  ensure => running,
+  enable => true,
+  require => [Package['apache2'], Package['php']]
+}
+package { 'mysql-server':
+  ensure => installed,
+}
+service { 'mysql':
+  ensure => running,
+}
+
+ 
+package { 'php5':
+  ensure => installed,
+}
+
+file { '/var/www/html/info.php':
+    ensure => file,
+    content => '',    # phpinfo code
+    require => Package['apache2'],        
+}
